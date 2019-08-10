fb7ce3076170b598aecb8f5dd4dc36b417c42027
Yan Gameiro <yan.gameiro@gmail.com>
2019-08-10 20:53:33 -0300

    adds button get a track

--- randomfy-react/src/components/get-a-track/index.js
+++ randomfy-react/src/components/get-a-track/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-const GetATrack = () => {
-    return (
-        <h1>get a track</h1>
-    );
-}
+export default class getATrack extends Component {
 
-export default GetATrack;
+    getATrack = () => {
+        console.log('i will get a track');
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>get a track</h1>
+                <button onClick={this.getATrack}>
+                    get a track
+                </button>
+            </div>
+        );
+    }
+}
