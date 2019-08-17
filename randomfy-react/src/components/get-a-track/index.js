04ac8a6d9ac33b00de9c2cceecb275081859f0e2
Yan Gameiro <yan.gameiro@gmail.com>
2019-08-17 14:04:38 -0300

    get a static track from spotfy

--- randomfy-react/src/components/get-a-track/index.js
+++ randomfy-react/src/components/get-a-track/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './styles.css';
 import api from '../../services/api';
+import authorization from "../../env"
 
 
 export default class getATrack extends Component {
 
     getATrack = async () => {
+
+        const config = { headers : {Authorization: authorization}}
         console.log('calling api...');
-        const result = await api.get(`/products?page=${1}`);
+        const result = await api.get(`tracks/7M6YvTJfLx3c8hiPlvCwl0`,config);
         console.log('result:');
         console.log(result);
     }
--- randomfy-react/src/services/api.js
+++ randomfy-react/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://rocketseat-node.herokuapp.com/api'
+  baseURL: 'https://api.spotify.com/v1'
 });
 
 export default api;
