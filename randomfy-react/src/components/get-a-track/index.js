655860415bd496b5a31184fc6612eab595bd0af3
Yan Gameiro <yan.gameiro@gmail.com>
2019-08-17 14:10:31 -0300

    refactor

--- randomfy-react/src/components/get-a-track/index.js
+++ randomfy-react/src/components/get-a-track/index.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import './styles.css';
 import api from '../../services/api';
-import authorization from "../../env"
 
 
 export default class getATrack extends Component {
 
     getATrack = async () => {
 
-        const config = { headers : {Authorization: authorization}}
         console.log('calling api...');
-        const result = await api.get(`tracks/7M6YvTJfLx3c8hiPlvCwl0`,config);
+        const result = await api.get(`tracks/7M6YvTJfLx3c8hiPlvCwl0`);
         console.log('result:');
-        console.log(result);
+        console.log(result.data);
     }
 
     render() {
--- randomfy-react/src/services/api.js
+++ randomfy-react/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import authorization from "../env"
+
+const config = {Authorization: authorization}
 
 const api = axios.create({
-  baseURL: 'https://api.spotify.com/v1'
+  baseURL: 'https://api.spotify.com/v1', headers: config
 });
 
 export default api;
