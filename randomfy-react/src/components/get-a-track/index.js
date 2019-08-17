d5564ebea73af444666cf63f906c3464576df369
Yan Gameiro <yan.gameiro@gmail.com>
2019-08-17 16:06:13 -0300

    show track name and artists

--- randomfy-react/src/components/get-a-track/index.js
+++ randomfy-react/src/components/get-a-track/index.js
@@ -4,6 +4,10 @@ import api from '../../services/api';
 
 
 export default class getATrack extends Component {
+    state = {
+        artists: [],
+        musicName: ""
+    }
 
     getATrack = async () => {
 
@@ -11,15 +15,32 @@ export default class getATrack extends Component {
         const result = await api.get(`tracks/7M6YvTJfLx3c8hiPlvCwl0`);
         console.log('result:');
         console.log(result.data);
+
+        this.setState({ 
+            musicName: result.data.name ,
+            artists: result.data.artists
+        })
     }
 
     render() {
+        const {musicName, artists} = this.state;
         return (
             <div>
                 <h1>get a track</h1>
                 <button onClick={this.getATrack}>
                     get a track
                 </button>
+                <ul>
+                    <li>Music Name: {musicName}</li>
+
+                    <p>Artists: </p>
+                    {artists.map(artist => {
+                        return (
+                            <li><a href={artist.external_urls.spotify}>{artist.name}</a></li>
+                        )
+                    })}
+
+                </ul>
             </div>
         );
     }
