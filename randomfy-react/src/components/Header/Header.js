6782fc6916a677c8aefc525af26fa302a8314b35
Paulino Jr <paulino.junior@outlook.com>
2019-08-21 20:27:29 -0300

    Added header

---
+++ randomfy-react/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeaderContainer } from './Header.styled'
+
+const header = (props) => {
+    return(
+        <HeaderContainer></HeaderContainer>
+    )   
+}
+
+export default header
---
+++ randomfy-react/src/components/Header/Header.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+    background-color: #212121;
+    height: 90px;
+    width: 100%;
+`
+
+export {HeaderContainer}
